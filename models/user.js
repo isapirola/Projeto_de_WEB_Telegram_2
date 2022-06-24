449543ee12875bea20d1ce91eb9871e1e61ebf2e
Isabela Pirola <belapirola@gmail.com>
2022-06-24 18:23:42 -0300

    user model

---
+++ models/user.js
@@ -0,0 +1,25 @@
+import mongoose from './config/db.config.js'
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+export const User = mongoose.model('User', UserSchema);

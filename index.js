23e67a3209bd03d1892c3c9c90b4cb705a0ad8cf
Isabela Pirola <belapirola@gmail.com>
2022-06-24 18:30:02 -0300

    move all main content to index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-import {app} from './setup.js'
+import express from "express";
+import { router } from "./controllers/authController.js";
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extends: false }))
+
+app.use('/auth', router)
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
--- routes.js
+++
@@ -1,8 +0,0 @@
-import Express from "express";
-import jwt from 'jsonwebtoken'
-
-export const router = new Express.Router()
-
-router.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
-});
